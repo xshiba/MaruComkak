52b8b6e74afe42a1b5986edf84d1378fcca5b760
xshiba <92438125+xshiba@users.noreply.github.com>
2022-12-22 19:35:19 +0700

    Update PCBit.lua

--- PCBit.lua
+++ PCBit.lua
@@ -1,5 +1,9 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/LoadMain.lua"))()
+    if syn and not KRNL_LOADED and not TRIGON_LOADED and not FLUXUS_LOADED then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/APISYNOB/main/Cream.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/LoadMain.lua"))()
+    end
 elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 or game.PlaceId == 6596144663 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/LOADLK.lua"))()
 elseif game.PlaceId == 6329844902 or game.PlaceId == 8396586868 or game.PlaceId == 6299805723 or game.PlaceId == 914010731 or game.PlaceId == 9790558424 then
