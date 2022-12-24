944f45d7e6c9ca5cef84345d561e98da2cd362f3
xshiba <92438125+xshiba@users.noreply.github.com>
2022-12-24 21:11:59 +0700

    Update PCBit.lua

--- PCBit.lua
+++ PCBit.lua
@@ -1,3 +1,4 @@
+repeat wait() until game:IsLoaded();
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/LoadMain.lua"))()
 elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 or game.PlaceId == 6596144663 then
