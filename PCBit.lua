da5f8a0c99eb908922e4d23a0f0b8da272fe88a0
xshiba <92438125+xshiba@users.noreply.github.com>
2023-04-19 02:35:09 +0700

    Update PCBit.lua

--- PCBit.lua
+++ PCBit.lua
@@ -1,10 +1,6 @@
 repeat wait() until game:IsLoaded();
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-    if getgenv().ScriptsMode == "BETAONEDAY" then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LOADBETA/main/ONEDAY.lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/LoadMain.lua"))()
-    end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/LoadMain.lua"))()
 elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 or game.PlaceId == 6596144663 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/LOADLK.lua"))()
 elseif game.PlaceId == 6329844902 or game.PlaceId == 12192552089 or game.PlaceId == 6299805723 or game.PlaceId == 914010731 or game.PlaceId == 9790558424 or game.PlaceId == 6918802270 then
