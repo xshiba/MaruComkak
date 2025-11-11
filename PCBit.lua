7f8393c0384ee7bfbea664310885fd9fb076e62e
xshiba <92438125+xshiba@users.noreply.github.com>
2025-11-11 21:09:28 +0700

    Update PCBit.lua

--- PCBit.lua
+++ PCBit.lua
@@ -40,7 +40,7 @@ task.spawn(function()
         end
     elseif MAPKL then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/Loader4.lua",true))()
-    elseif (MAPBLADEB or MAPFRUITWARRIOR or MAPAFS or MAPRGH or MAPHAZEP or MAPALS or DRIVEEMPIRE or DEMONPIECE or SOLRNG or TITAN or AD or MS or AV or PG or Fish or Jujutsu or AA or BL or AC or HT or ARX or BS or GAG or ASTDX or days99 or ZOmBie or Fishit or BAZ) then
+    elseif (MAPBLADEB or MAPFRUITWARRIOR or MAPAFS or MAPRGH or MAPHAZEP or MAPALS or DRIVEEMPIRE or DEMONPIECE or SOLRNG or TITAN or AD or MS or AV or PG or Fish or Jujutsu or AA or BL or AC or HT or ARX or BS or GAG or ASTDX or days99 or ZOmBie or game.GameId == 6701277882 or BAZ) then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/LoveCream/main/Loader3.lua",true))()
     else
         game.Players.LocalPlayer:Kick("[ MARU ] NOT SUPPORT")
